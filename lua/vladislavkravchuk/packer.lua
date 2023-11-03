c0c479197f0df2fb30b4507bf148e90ee84a2b2d
kravchuk21 <krauchukvlad@gmail.com>
2023-11-03 12:36:55 +0300

    add `nvim-treesitter`
    - https://github.com/nvim-treesitter/nvim-treesitter

--- lua/vladislavkravchuk/packer.lua
+++ lua/vladislavkravchuk/packer.lua
@@ -14,4 +14,9 @@ return require('packer').startup(function(use)
   }
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
+
+  use( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+
+  use('ThePrimeagen/harpoon')
 end)
